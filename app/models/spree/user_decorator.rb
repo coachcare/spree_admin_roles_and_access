4a41a52cf9304213885b9d816328a4903ba1b9d9
umairrazam <omairrr.azam@gmail.com>
2021-09-03 21:06:30 +0500

    fix(decorator): syntax

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,8 +1,8 @@
-module SpreeAdminRolesAndAccess
+module Spree
   module UserDecorator
 		def self.prepended(base)
 			base.alias_attribute :roles, :spree_roles
 		end	
 	end
 end
-Spree.user_class.prepend SpreeAdminRolesAndAccess::UserDecorator 
+Spree.user_class.prepend Spree::UserDecorator 
